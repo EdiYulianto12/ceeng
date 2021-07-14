98d64b543f8b8ed044c5206f5e8a1552eb736abc
Edi Yulianto <84266203+EdiYulianto12@users.noreply.github.com>
2021-07-15 00:07:54 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 4 Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: white;
+  }
+  .npm{
+    text-indent: 18px;
+  }
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>5 Fakta Tersembunyi Game PUBG yang Jarang Diketahui Pecinta Game!</h1>
+   
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.php"><font face="Copperplate Gothic Bold">ParaGamer</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>    
+    </ul>
+  </div>  
+</nav><br>
+<h3><center><b>About</b></center></h3>
+<div class="tim"><b><h3>Tim Perancang Web Desain</div></h3></b>
+<h4>1. Nama : Edi Yulianto</h4>
+  <h4 class="npm">NPM  &nbsp;&nbsp;: 1955201090 <br></h4>
+  <h4>2. Nama : Ronny Saputra</h4>
+  <h4 class="npm">NPM  &nbsp;&nbsp;: 1955201093<br></h4>
+  <h4>3. Nama : Safan Masera Buana Paksi</h4>
+  <h4 class="npm">NPM  &nbsp;&nbsp;: 1955201085 <br></h4>
+  <h4>4. Nama : Ryan Rahmat Kurniawan</h4>
+  <h4 class="npm">NPM  &nbsp;&nbsp;: 1955201102<br></h4><br>
+<div class="ket"><h4> Tugas Analisa Dan Perancangan Sistem Universitas Muhammadiyah Bengkulu</div></h4>
+
+<div  style="margin-bottom:0; height: 60px; background-color: lightblue; text-align: center;">
+  <b><p style="font-size: 40px; font-family: sans-serif;">@2021 || Para Gamer</p></b>
+</div>
+</body>
+</html
---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 4 Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: white;
+  }
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>5 Fakta Tersembunyi Game PUBG yang Jarang Diketahui Pecinta Game!</h1>
+   
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.php"><font face="Copperplate Gothic Bold">ParaGamer</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>About Me</h2>
+     
+      <div class="fakeimg"><center><img src="foto.jpg" height="200" width="300"></center></div><br>
+      <h6 style="text-indent: 23px;">Game adalah kebutuhan hidup kami</h6>
+    </div>
+    <div class="col-sm-8">
+      <h2> Playerunknown's Battlegrounds (PUBG)</h2>
+      <h5>PUBG,10 Juli 2021</h5>
+      <div class="fakeimg"><center><img src="lan.jpg" width="400" height="200"></div></center><br>
+      <p>Kira-kira apa aja? Ada yang bisa tebak?</p>
+      <p>Siapa sih yang tak kenal dengan game Playerunknown's Battlegrounds (PUBG), yang bergenre battle royale ini? semua gamers pastinya tahu dan kemungkinan besar pernah memainkan game yang satu ini. Game ini sedang booming di tahun 2018, khususnya di Indonesia. siapa sangka, ternyata di balik ketenaran game ini, ada beberapa fakta tersembunyi game PUBG yang belum diketahui oleh banyak gamers.</p>
+      <br>
+      <h2>1. Tragedi sebutan Chicken Dinner</h2>
+      
+      <div class="fakeimg"><center><img src="ckn.jpg" width="450" height="200"></center></div><br>
+      <p>Chicken Dinner</p>
+      <p>Jika pemain bisa bertahan hingga akhir dan menjadi yang terakhir di match, maka akan ada tulisan yang menyebutkan chicken dinner. Banyak para pecinta game yang belum mengetahui tragedi dari sebutan chicken dinner.
+
+Chicken dinner itu bermula pada masa great depression, yang terjadi di Amerika Serikat sekitar tahun 1930. Di masa ini, banyak yang melakukan perjudian untuk satu tujuan, ialah mendapatkan uang sebanyak mungkin. Jika menang berturut turut, maka orang tersebut bisa membeli ayam untuk makan malam. Saat itu beli ayam sudah termasuk mewah banget. Nah, ini sejarah dari adanya sebutan chicken dinner di PUBG.</p>
+   
+    <br>
+      <h2>2. Pencipta PUBG ternyata bukan gamer berat</h2>
+      
+      <div class="fakeimg"><center><img src="pct.jpg" width="450" height="200"></center></div><br>
+    
+      <p>Gak percaya? Pengakuan ini langsung dari pencipta PUBG yaitu Brendan Genne. Menurutnya, ia bukanlah seorang gamer. Brendan genner mengaku main game sesempatnya saja jika ada waktu luang. Tak disangka ia malah menciptkan game PUBG, yang hingga sekarang menjadi populer dan disukai oleh para pecinta game.</p>
+       <br>
+      <h2>3. PUBG menghapus banyak pemain yang curang</h2>
+      
+      <div class="fakeimg"><center><img src="3.jpg" width="450" height="200"></center></div><br>
+    PUBG sudah banyak menghapus akun dari pemain yang menggunakan cheat dalam permainannya. Di tahun 2017 saja sudah lebih dari satu juta akun yang dihapus. Bahkan di tahun 2018 juga masih banyak pemain yang terpaksa harus dihapus oleh pihak PUBG, karena telah bermain curang.</p>
+     <br>
+      <h2>4. Investor ragu saat awal pengembangan PUBG</h2>
+      
+      <div class="fakeimg"><center><img src="4.jpg" width="450" height="200"></center></div><br>
+   Seorang investor memiliki keraguan dalam bekerja sama dengan pemilik game PUBG karena ia berkewarganegaraan asing. Brendan Genne adalah orang irlanda, sedangkan calon investornya adalah orang korea selatan. Karena inilah investor ragu dalam menanamkan modalnya. Ia menganggap bekerja sama dengan orang asing akan mengundang risiko yang besar.</p>
+    <br>
+      <h2>5. Asal dari nama PlayerUnknown</h2>
+      
+      <div class="fakeimg"><center><img src="5.jpg" width="450" height="200"></center></div><br>
+   Asal nama dari PlayerUnknown diambil dari nickname si pembuat game PUBG, yaitu Brendan Grenne. Jadi begini ceritanya... Di saat kecil, Brendan sedang bermain game dengan temannya. Sewaktu bermain itu, ia mengganti nama servernya yang awalnya player 1, menjadi PlayerUnknown, sejak itu nama itu terus dipakai.
+
+Nah itu dia 5 fakta tersembunyi game PUBG yang belum diketahui oleh banyak pecinta game PUBG. Semoga artikel ini bisa bermanfaat bagi para pecinta game PUBG.</p>
+    </div>
+    </div>
+  </div>
+</div>
+<br><br>
+<div  style="margin-bottom:0; height: 60px; background-color: lightblue; text-align: center;">
+  <b><p style="font-size: 40px; font-family: sans-serif;">@2021 || Para Gamer</p></b>
+</div>
+
+</body>
+</html>
